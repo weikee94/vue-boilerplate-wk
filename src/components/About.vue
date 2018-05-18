608c059b2210966cb49e068c3b6181aec3a5cbdd
WeiKee <weikee070194@gmail.com>
2018-05-18 19:08:28 +0800

    init vue project

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <p>About</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      msg: 'About'
+    }
+  }
+}
+</script>
